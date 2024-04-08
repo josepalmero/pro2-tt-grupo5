436c947a73bd9d6f0aac07a63ecc04fca43e8933
Josefina Palmero <josepalmero@MacBook-Air-de-Josefina-4.local>
2024-04-08 16:45:48 -0300

    tabla usuarios y productos

---
+++ myapp/db/data.sql
@@ -0,0 +1,30 @@
+CREATE SCHEMA my_db;
+
+USE my_db;
+
+CREATE TABLE usuarios (
+/* 	nombreColumna 		tipoDato 		Restricciones */
+    id 					INT 			UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    email			    VARCHAR(250) 	NOT NULL,
+    contrasenia 	    VARCHAR(250) 	NOT NULL,		 	
+    fecha               DATE 			NOT NULL,
+    dni                 INT 			NOT NULL,
+    foto                VARCHAR(250) 	NOT NULL,
+    createdAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ,
+	updatedAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt           TIMESTAMP       ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE productos (
+/* 	nombreColumna 		tipoDato 		Restricciones */
+    id 					INT 			UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	foto                VARCHAR(250) 	NOT NULL,
+    nombre              VARCHAR(250) 	NOT NULL,
+    descripcion         VARCHAR(250) 	NOT NULL,
+    createdAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ,
+	updatedAt 			TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt           TIMESTAMP       ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+
