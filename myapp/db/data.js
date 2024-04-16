11428f3de68000a012c906a912c4f45dc2ce2abe
Serena Fabbian <serenafabbian@MacBook-Air-de-Serena.local>
2024-04-16 19:46:11 -0300

    product

--- myapp/db/data.js
+++ myapp/db/data.js
@@ -22,12 +22,12 @@ let data = {
         id: 2 , 
         foto: '/images/products/Borcego_dip.png' ,
         nombre: 'Borcego Dip' ,
-        descripcion: 'Borcegos de ecocuero , base de 2,5 cm, altura de caña de 28 cm, y 37 cm de circiunferencia.' ,
+        descripcion: 'Borcegos de cuero con tachas doradas, base de 2,5 cm, altura de caña de 28 cm, y 37 cm de circiunferencia.' ,
     }, {
         id: 3 , 
         foto: '/images/products/Borcego_mex.png' ,
         nombre: 'Borcego Mex' ,
-        descripcion: 'Borcegos de cuero con hebillas negras. Altura de 21 cm, taco de 4 cm.' ,
+        descripcion: 'Borregos de cuero, con apliques plateados, taco de 5,5 cm. ' ,
     }, {
         id: 4 , 
         foto: '/images/products/Bota_aliss_marron.png' ,
@@ -42,12 +42,32 @@ let data = {
         id: 6 , 
         foto: '/images/products/Texana_max.png' ,
         nombre: 'Texana Max' ,
-        descripcion: 'Texanas de cuero con bordado, altura de la caña 21,5 cm. alto de taco 4 cm.' ,
+        descripcion: 'Texanas de ecocuero, alto de caña 22 cm, taco de 4 cm de alto.',
     }, {
         id: 7 , 
         foto: '/images/products/Texana_only.png' ,
         nombre: 'Texana Only' ,
         descripcion: 'Texanas de cuero, con bordado y hebilla. Alto de la caña 29 cm, alto de taco 4 cm.' ,
+    },{
+        id: 8, 
+        foto: '/images/products/Texana_cow.png' ,
+        nombre: 'Texana cow' ,
+        descripcion: 'Botas negras de cuero combinadas en contratono beige. Altura de la caña 36,5 cm. y altura del taco 7 cm.' ,
+    },{
+        id: 9 , 
+        foto: '/images/products/Bota_aliss_negra.png' ,
+        nombre: 'Bota Aliss Negra' ,
+        descripcion: 'Borcegos de cuero, con hebillas en el costado, alto de caña de 30,5 cm y taco de 5 cm.' ,
+    },{
+        id: 10 , 
+        foto: '/images/products/Borcego_tiki.png' ,
+        nombre: 'Borcego Tiki' ,
+        descripcion: 'Borcegos de ecocuero y de charol opaco, ded caña alta.' ,
+    },{
+        id: 11 , 
+        foto: '/images/products/Bota_clasica.png' ,
+        nombre: 'Bota Clasica' ,
+        descripcion: 'Borcegos de cuero, con caña de 40 cm de alto y base de 5,5 cm de alto.' ,
     },],
 
     comentarios: {
