8e340586552db93b0010927ac7adf55c78394588
Josefina Palmero <josepalmero@MacBook-Air-de-Josefina-4.local>
2024-04-18 20:31:48 -0300

    data js

--- myapp/db/data.js
+++ myapp/db/data.js
@@ -13,81 +13,254 @@ let data = {
         foto: '/images/products/Borcego_aliss.png' ,
         nombre: 'Borcego Aliss' ,
         descripcion: 'Borcego de cuero, con apliques de color plateado. Alto de caña de 27 cm.' ,
+        comentarios: [{
+            nombre_de_usuario: "serefabb",
+            texto: "Me encantaron las botas!!!",
+            foto_de_perfil: "/images/usuarios/loli.JPG",
+            },
+            {
+            nombre_de_usuario: "josepal",
+            texto: "Muy lindo calzado y super canchero",
+            foto_de_perfil: "/images/usuarios/anita.PNG",
+            },
+            {
+            nombre_de_usuario: "franfa",
+            texto: "Usos sus borcegos siempree",
+            foto_de_perfil: "/images/usuarios/alan.JPG",
+            }
+        ]
     }, {
         id: 1, 
         foto: '/images/products/Borcego_cordon.png' ,
         nombre: 'Borcego Cordon' ,
         descripcion: 'Borcego de cuero con una caña de 23,5 cm de largo, base de 7 cm de taco y cordones largos.' ,
+        comentarios: [{
+            nombre_de_usuario: "mairagomez_",
+            texto: "Hermosos calzados!! Los colecciono",
+            foto_de_perfil: "/images/usuarios/loli.JPG",
+            },
+            {
+            nombre_de_usuario: "mario_rodrigez",
+            texto: "Quiero todo",
+            foto_de_perfil: "/images/usuarios/mariano.JPG",
+            },
+            {
+            nombre_de_usuario: "pedro.lopez",
+            texto: "Amo Sefina",
+            foto_de_perfil: "/images/usuarios/pedro.JPG",
+            }
+        ]
     }, {
         id: 2 , 
         foto: '/images/products/Borcego_dip.png' ,
         nombre: 'Borcego Dip' ,
         descripcion: 'Borcegos de cuero con tachas doradas, base de 2,5 cm, altura de caña de 28 cm, y 37 cm de circiunferencia.' ,
+        comentarios: [{
+            nombre_de_usuario: "sanchez.oli",
+            texto: "AMOO",
+            foto_de_perfil: "/images/usuarios/anita.PNG",
+            },
+            {
+            nombre_de_usuario: "anita725",
+            texto: "Me encantan todas!!!",
+            foto_de_perfil: "/images/usuarios/loli.JPG",
+            },
+            {
+            nombre_de_usuario: "franfa",
+            texto: "Usos sus borcegos siempree",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            }
+        ]
     }, {
         id: 3 , 
         foto: '/images/products/Borcego_mex.png' ,
         nombre: 'Borcego Mex' ,
         descripcion: 'Boregos de cuero, con apliques plateados, taco de 5,5 cm. ' ,
+        comentarios: [{
+            nombre_de_usuario: "serefabb",
+            texto: "Me encantaron las botas!!!",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "josepal",
+            texto: "Muy lindo calzado y super canchero",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "franfa",
+            texto: "Usos sus borcegos siempree",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            }
+        ]
     }, {
         id: 4 , 
         foto: '/images/products/Bota_aliss_marron.png' ,
         nombre: 'Bota Aliss Marron' ,
         descripcion: 'Borcegos de cuero, con hebillas en el costado, alto de caña de 30,5 cm y taco de 5 cm.' ,
+        comentarios: [{
+            nombre_de_usuario: "serefabb",
+            texto: "Me encantaron las botas!!!",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "josepal",
+            texto: "Muy lindo calzado y super canchero",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "franfa",
+            texto: "Usos sus borcegos siempree",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            }
+        ]
     }, {
         id: 5 , 
         foto: '/images/products/Bota_owen.png' ,
         nombre: 'Bota Owen' ,
         descripcion: 'Borcegos de cuero con hebillas, apliques de metal de caña alta y taco de 5 cm. ',
+        comentarios: [{
+            nombre_de_usuario: "serefabb",
+            texto: "Me encantaron las botas!!!",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "josepal",
+            texto: "Muy lindo calzado y super canchero",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "franfa",
+            texto: "Usos sus borcegos siempree",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            }
+        ]
     }, {
         id: 6 , 
         foto: '/images/products/Texana_max.png' ,
         nombre: 'Texana Max' ,
         descripcion: 'Texanas de ecocuero, alto de caña 22 cm, taco de 4 cm de alto.',
+        comentarios: [{
+            nombre_de_usuario: "serefabb",
+            texto: "Me encantaron las botas!!!",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "josepal",
+            texto: "Muy lindo calzado y super canchero",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "franfa",
+            texto: "Usos sus borcegos siempree",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            }
+        ]
     }, {
         id: 7 , 
         foto: '/images/products/Texana_only.png' ,
         nombre: 'Texana Only' ,
         descripcion: 'Texanas de cuero, con bordado y hebilla. Alto de la caña 29 cm, alto de taco 4 cm.' ,
+        comentarios: [{
+            nombre_de_usuario: "serefabb",
+            texto: "Me encantaron las botas!!!",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "josepal",
+            texto: "Muy lindo calzado y super canchero",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "franfa",
+            texto: "Usos sus borcegos siempree",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            }
+        ]
     },{
         id: 8, 
         foto: '/images/products/Texana_cow.png' ,
         nombre: 'Texana cow' ,
         descripcion: 'Texana negras de cuero combinadas en contratono beige. Altura de la caña 36,5 cm. y altura del taco 7 cm.' ,
+        comentarios: [{
+            nombre_de_usuario: "serefabb",
+            texto: "Me encantaron las botas!!!",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "josepal",
+            texto: "Muy lindo calzado y super canchero",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "franfa",
+            texto: "Usos sus borcegos siempree",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            }
+        ]
     },{
         id: 9 , 
         foto: '/images/products/Bota_aliss_negra.png' ,
         nombre: 'Bota Aliss Negra' ,
         descripcion: 'Borcegos de cuero, con hebillas en el costado, alto de caña de 30,5 cm y taco de 5 cm.' ,
+        comentarios: [{
+            nombre_de_usuario: "serefabb",
+            texto: "Me encantaron las botas!!!",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "josepal",
+            texto: "Muy lindo calzado y super canchero",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "franfa",
+            texto: "Usos sus borcegos siempree",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            }
+        ]
     },{
         id: 10 , 
         foto: '/images/products/Borcego_tiki.png' ,
         nombre: 'Borcego Tiki' ,
         descripcion: 'Borcegos de ecocuero y de charol opaco, de caña alta.' ,
+        comentarios: [{
+            nombre_de_usuario: "serefabb",
+            texto: "Me encantaron las botas!!!",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "josepal",
+            texto: "Muy lindo calzado y super canchero",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            },
+            {
+            nombre_de_usuario: "franfa",
+            texto: "Usos sus borcegos siempree",
+            foto_de_perfil: "/images/foto-perfil.webp",
+            }
+        ]
     },{
         id: 11 , 
         foto: '/images/products/Bota_clasica.png' ,
         nombre: 'Bota Clasica' ,
         descripcion: 'Borcegos de cuero, con caña de 40 cm de alto y base de 5,5 cm de alto.' ,
-    },
-    {comentarios: [{
+        comentarios: [{
             nombre_de_usuario: "serefabb",
             texto: "Me encantaron las botas!!!",
             foto_de_perfil: "/images/foto-perfil.webp",
-
-        },{
+            },
+            {
             nombre_de_usuario: "josepal",
             texto: "Muy lindo calzado y super canchero",
             foto_de_perfil: "/images/foto-perfil.webp",
-        },{
+            },
+            {
             nombre_de_usuario: "franfa",
             texto: "Usos sus borcegos siempree",
             foto_de_perfil: "/images/foto-perfil.webp",
-        },{
-            nombre_de_usuario: "verob",
-            texto: "Cuando vuevle a ingresar los Borcegos Aliss??",
-            foto_de_perfil: "/images/foto-perfil.webp",
-        },
-    ]},
+            }
+        ]
+    },
 ],};
 
 module.exports = data
