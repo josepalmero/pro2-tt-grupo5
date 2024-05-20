8a74ea480a881b3ba602987b3a3ef01f8e25711d
Serena Fabbian <serenafabbian@MacBook-Air-de-Serena.local>
2024-05-20 15:15:12 -0300

    express session

--- myapp/app.js
+++ myapp/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +23,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/*Configuracion para session */
+app.use(session({
+  secret: "myapp",
+  resave: false,
+  saveUninitialized: true,
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
