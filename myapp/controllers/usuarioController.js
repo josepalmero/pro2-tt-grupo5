a91a37e8a9a292dfa5b577328de969b1281ec2f3
Serena Fabbian <serenafabbian@MacBook-Air-de-Serena.local>
2024-06-10 17:29:27 -0300

    usuario

--- myapp/controllers/usuarioController.js
+++ myapp/controllers/usuarioController.js
@@ -1,54 +1,57 @@
+const { where } = require("sequelize");
 const data = require("../database/models");
 const op = data.Sequelize.Op;
 
 
 const usuarioController = {
     login: function (req, res) {
-        data.Usuario.create()
-        .then(function(result){
-            return res.render("login", {usuario: result});
-        })
-        .catch(function(err){
-            return console.log(err);
-        });
-       /*res.render('login')*/
+        data.Usuario.findByPk()
+            .then(function (result) {
+                return res.render("login", { usuario: result });
+            })
+            .catch(function (err) {
+                return console.log(err);
+            });
+        /*res.render('login')*/
     },
-    
+
     register: function (req, res) {
-        data.Usuario.create()
-        .then(function(result){
-            return res.render("register", {usuario: result});
-        })
-        .catch(function(err){
-            return console.log(err);
-        });
+        data.Usuario.findByPk()
+            .then(function (result) {
+                return res.render("register", { usuario: result });
+            })
+            .catch(function (err) {
+                return console.log(err);
+            });
         /*res.render('register');*/
     },
 
-    profile:  function (req, res) {
-        data.Usuario.create()
-        .then(function(result){
-            return res.render("profile", {usuario: data.usuario, 
-                productos: data.productos});
-        })
-        .catch(function(err){
-            return console.log(err);
-        });
-        
+    profile: function (req, res) {
+        data.Usuario.findByPk()
+            .then(function (result) {
+                return res.render("profile", {
+                    usuario: data.usuario,
+                    productos: data.productos
+                });
+            })
+            .catch(function (err) {
+                return console.log(err);
+            });
+
         /*res.render('profile' , {usuario: data.usuario, 
             productos: data.productos
         });*/
     },
-    
-    profile_edit: function(req, res){
-        data.Usuario.create()
-        .then(function(result){
-            return res.render("profile-edit", {usuario: result});
-        })
-        .catch(function(err){
-            return console.log(err);
-        });
-        
+
+    profile_edit: function (req, res) {
+        data.Usuario.findByPk()
+            .then(function (result) {
+                return res.render("profile-edit", { usuario: result });
+            })
+            .catch(function (err) {
+                return console.log(err);
+            });
+
         /*res.render('profile-edit');*/
     },
 };
