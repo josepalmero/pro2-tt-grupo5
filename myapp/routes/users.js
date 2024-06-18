2a036b0d9b90b7c6ee1bbb395b1085db8799d365
Ignacio Merljak <imerljak@udesa.edu.ar>
2024-06-18 17:10:40 -0300

    Continuacion de las validaciones registro

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -3,9 +3,33 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const {body} = require('express-validator');
 const validations= [
+  body("email")
+  .notEmpty().withMessage("Este campo no puede estar vacio").bail()
+  .isEmail()
+  .custom(function(value){
+    return db.data.findOne({
+      where: {email: value},
+    })
+    .then(function(user){
+      if(user){
+        throw new Error ('El email ingresado ya existe.')
+      }
+    })
+  }),
   body("name")
-  .notEmpty().withMessage("Este campo es obligatorio").bail()
-]
+  .notEmpty().bail(),
+  body("password")
+  .notEmpty().withMessage("Este campo no puede estar vacio").bail()
+  .isLength({min:4}).withMessage("La contrasenia debe tener al menos 4 caracteres"),// me falta encryptar
+  body("fechaNacimiento")
+  .isDate(),
+  body("documento")
+  .isInt(),
+  body("fotoPerfil")
+  ];
+
+  
+  
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
